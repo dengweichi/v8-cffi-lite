35002b4b656f3a10bc2267033420024de21589ae
dengweichi@cmcm.com <dengweichi@cmcm.com>
2021-06-23 19:29:25 +0800

    feat:初始化项目

---
+++ README.md
@@ -0,0 +1,3 @@
+# v8-ffi
+
+使用lin-cffi和v8实现javaScript导入共享库
---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include "v8.h"
+#include "cffi.h"
+#include "libplatform/libplatform.h"
+
+int main(int argc, char** argv){
+    v8::V8::InitializeICUDefaultLocation(argv[0]);
+    v8::V8::InitializeExternalStartupData(argv[0]);
+    std::unique_ptr<v8::Platform> platform = v8::platform::NewDefaultPlatform();
+    v8::V8::InitializePlatform(platform.get());
+    v8::V8::Initialize();
+    v8::Isolate::CreateParams create_params;
+    create_params.array_buffer_allocator = v8::ArrayBuffer::Allocator::NewDefaultAllocator();
+    v8::Isolate* isolate = v8::Isolate::New(create_params);
+    {
+        v8::Isolate::Scope isolate_scope(isolate);
+        v8::HandleScope handle_scope(isolate);
+        v8::Local<v8::Context> context = v8::Context::New(isolate);
+        v8::Context::Scope context_scope(context);
+    }
+    isolate->Dispose();
+    v8::V8::Dispose();
+    v8::V8::ShutdownPlatform();
+    delete create_params.array_buffer_allocator;
+    return 0;
+}
